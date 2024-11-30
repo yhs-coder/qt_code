edae897a1ccfc7ed672fe24ff1725b6949e5b644
yhs-coder <yhs6666@outlook.com>
2024-11-30 23:30:08 +0800

    QSlider

--- src/QSlider/widget.h
+++ src/QSlider/widget.h
@@ -16,6 +16,13 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+    QString getRGBValue();    // 返回颜色值
+    void setBackgroupColor(); // 更新背景颜色
+
+private slots:
+    void on_hsRed_valueChanged(int value);
+    void on_hsGreen_valueChanged(int value);
+    void on_hsBlue_valueChanged(int value);
 
 private:
     Ui::Widget *ui;
