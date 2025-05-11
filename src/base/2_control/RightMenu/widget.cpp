c3f83e3151c840588ec835c572bab8491fc00ae0
yhs-coder <yhs6666@outlook.com>
2025-05-11 23:38:34 +0800

    右键菜单

---
+++ src/base/2_control/RightMenu/widget.cpp
@@ -0,0 +1,27 @@
+﻿#include "widget.h"
+
+#include <QAction>
+#include <QMessageBox>
+
+#include "./ui_widget.h"
+
+Widget::Widget(QWidget* parent) : QWidget(parent), ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    // 设置菜单策略
+    // Qt::DefaultContextMenu: the widget's QWidget::contextMenuEvent() handler is called.
+    this->setContextMenuPolicy(Qt::DefaultContextMenu);
+
+    initMenu();
+}
+
+Widget::~Widget() { delete ui; }
+
+void Widget::contextMenuEvent(QContextMenuEvent* event)
+{
+    // 在点击的位置弹出菜单
+    m_pMenu->exec(QCursor::pos());
+}
+
+
---
+++ src/base/2_control/RightMenu/widget.h
@@ -0,0 +1,30 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QMenu>
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget* parent = nullptr);
+    ~Widget();
+
+    // 菜单事件
+    void contextMenuEvent(QContextMenuEvent* event) override;
+
+    void initMenu();
+
+private:
+    Ui::Widget* ui;
+    QMenu*      m_pMenu = nullptr;
+};
+#endif  // WIDGET_H
