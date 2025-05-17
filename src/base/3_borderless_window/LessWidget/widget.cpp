8037b0aa8b4ac4ae0c7412af2368ab8ad62b4614
yhs-coder <yhs6666@outlook.com>
2025-05-17 23:46:53 +0800

    无边框窗口，按住鼠标可移动窗口

---
+++ src/base/3_borderless_window/LessWidget/widget.cpp
@@ -0,0 +1,20 @@
+﻿#include "widget.h"
+
+#include <QMouseEvent>
+
+Widget::Widget(QWidget *parent) : QWidget(parent) { this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowMinMaxButtonsHint); }
+
+Widget::~Widget() {}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+    mouse_pos  = event->globalPos();
+    window_pos = this->pos();
+    diff_pos   = mouse_pos - window_pos;
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    QPoint pos = event->globalPos();
+    this->move(pos - diff_pos);
+}
---
+++ src/base/3_borderless_window/LessWidget/widget.h
@@ -0,0 +1,23 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget* parent = nullptr);
+    ~Widget();
+
+private:
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+
+private:
+    QPoint diff_pos;  // 鼠标和窗口的相对位移
+    QPoint window_pos;
+    QPoint mouse_pos;
+};
+#endif  // WIDGET_H
