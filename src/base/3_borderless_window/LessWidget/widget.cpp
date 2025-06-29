9d386437decd27354b94cd99b413480bf567d612
yhs-coder <yhs6666@outlook.com>
2025-06-29 14:41:36 +0800

    增加内容

--- src/base/3_borderless_window/LessWidget/widget.cpp
+++ src/base/3_borderless_window/LessWidget/widget.cpp
@@ -8,13 +8,18 @@ Widget::~Widget() {}
 
 void Widget::mousePressEvent(QMouseEvent *event)
 {
-    mouse_pos  = event->globalPos();
+    // 鼠标位置相对于桌面原点的绝对位置
+    mouse_pos = event->globalPos();
+    // 程序窗口左上角位置相对于桌面原点的绝对位置
     window_pos = this->pos();
-    diff_pos   = mouse_pos - window_pos;
+    // 鼠标与窗口位置的相对路径
+    diff_pos = mouse_pos - window_pos;
 }
 
 void Widget::mouseMoveEvent(QMouseEvent *event)
 {
+    // 获取鼠标移动后的位置
     QPoint pos = event->globalPos();
+    // 鼠标新位置减去与窗口的相对路径
     this->move(pos - diff_pos);
 }
