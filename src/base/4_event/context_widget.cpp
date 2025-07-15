9835973d7d1397aa508b9819a34519acd7e278a1
yhs-coder <yhs6666@outlook.com>
2025-07-15 18:39:46 +0800

    右键事件

--- src/base/4_event/context_widget.cpp
+++ src/base/4_event/context_widget.cpp
@@ -1,23 +1,52 @@
 ﻿#include "context_widget.h"
 
+#include <QContextMenuEvent>
+#include <QDebug>
 #include <QLabel>
+#include <QMenu>
 #include <QVBoxLayout>
+
 ContextWidget::ContextWidget(QWidget* parent) : QWidget{parent}
 {
-    QVBoxLayout* verticalLayout = new QVBoxLayout(this);
-    verticalLayout->setSpacing(0);
-    verticalLayout->setContentsMargins(0, 0, 0, 0);
+    m_cut     = new QAction("剪切(T)", this);
+    m_copy    = new QAction("复制(C)", this);
+    m_paste   = new QAction("粘贴(P)", this);
+    m_toUpper = new QAction("转成大写(U)", this);
+    m_toLower = new QAction("转成小写(L)", this);
+    m_hide    = new QAction("隐藏行", this);
 
-    QLabel* label = new QLabel(this);
-    label->setText("右键事件");
-    label->setFrameShape(QFrame::Box);
-    label->setFixedHeight(60);
-    label->setAlignment(Qt::AlignCenter);
-    label->setStyleSheet(R"(
-        background-color: blue;
-        color: white;
-        font-size: 25px
-    )");
+    connect(m_cut, &QAction::triggered, this, &ContextWidget::slotAction);
+    connect(m_copy, &QAction::triggered, this, &ContextWidget::slotAction);
+    connect(m_paste, &QAction::triggered, this, &ContextWidget::slotAction);
+    connect(m_toUpper, &QAction::triggered, this, &ContextWidget::slotAction);
+    connect(m_toLower, &QAction::triggered, this, &ContextWidget::slotAction);
+    connect(m_hide, &QAction::triggered, this, &ContextWidget::slotAction);
+}
 
-    verticalLayout->addWidget(label);
+void ContextWidget::slotAction()
+{
+    // sender()返回指向发出信号对象的指针
+    QAction* act = (QAction*)sender();
+#if 0
+   if ( act == m_cut ) {
+        qDebug() << "slot_cut";
+    }
+#endif
+    qDebug() << act->text();
+}
+
+void ContextWidget::contextMenuEvent(QContextMenuEvent* event)
+{
+    QMenu* menu = new QMenu();
+    menu->setFixedWidth(200);
+    menu->addAction(m_cut);
+    menu->addAction(m_copy);
+    menu->addAction(m_paste);
+    menu->addSeparator();
+    menu->addAction(m_toUpper);
+    menu->addAction(m_toLower);
+    menu->addSeparator();
+    menu->addAction(m_hide);
+    menu->exec(event->globalPos());
+    delete menu;
 }
--- src/base/4_event/context_widget.h
+++ src/base/4_event/context_widget.h
@@ -1,15 +1,28 @@
 ﻿#ifndef CONTEXT_WIDGET_H
 #define CONTEXT_WIDGET_H
 
+#include <QAction>
 #include <QWidget>
 
 class ContextWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit ContextWidget(QWidget *parent = nullptr);
+    explicit ContextWidget(QWidget* parent = nullptr);
 
-signals:
+private slots:
+    void slotAction();
+
+protected:
+    void contextMenuEvent(QContextMenuEvent* event) override;
+
+private:
+    QAction* m_cut;
+    QAction* m_copy;
+    QAction* m_paste;
+    QAction* m_toUpper;
+    QAction* m_toLower;
+    QAction* m_hide;
 };
 
-#endif // CONTEXT_WIDGET_H
+#endif  // CONTEXT_WIDGET_H
