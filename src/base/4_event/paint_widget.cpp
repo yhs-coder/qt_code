cf072424303f4104b9426cce7befedcb99ce8e3b
yhs-coder <yhs6666@outlook.com>
2025-07-15 15:50:50 +0800

    绘图事件，绘制高低温度曲线

--- src/base/4_event/paint_widget.cpp
+++ src/base/4_event/paint_widget.cpp
@@ -1,7 +1,18 @@
 ﻿#include "paint_widget.h"
 
+#include <QDebug>
 #include <QLabel>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QRandomGenerator>
 #include <QVBoxLayout>
+
+// 温度曲线相关的宏
+#define PADDING 50
+#define INCREMENT 8       // 温度曲线像素增量
+#define POINT_RADIUS 4    // 曲线描点的大小
+#define TEXT_OFFSET_X 12  // 温度文本相对于点的偏移
+#define TEXT_OFFSET_Y 10  // 温度文本相对于点的偏移
 
 PaintWidget::PaintWidget(QWidget* parent) : QWidget{parent}
 {
@@ -9,16 +20,179 @@ PaintWidget::PaintWidget(QWidget* parent) : QWidget{parent}
     verticalLayout->setSpacing(0);
     verticalLayout->setContentsMargins(0, 0, 0, 0);
 
-    QLabel* label = new QLabel(this);
-    label->setText("绘图事件");
-    label->setFrameShape(QFrame::Box);
-    label->setFixedHeight(60);
-    label->setAlignment(Qt::AlignCenter);
-    label->setStyleSheet(R"(
-        background-color: blue;
-        color: white;
-        font-size: 25px
-    )");
+    // 添加一个label，用于绘制高温曲线
+    m_lblHigh = new QLabel(this);
+    m_lblHigh->setText("");
+    m_lblHigh->setFrameShape(QFrame::Box);
 
-    verticalLayout->addWidget(label);
+    // 添加一个label，用于绘制低温曲线
+    m_lblLow = new QLabel(this);
+    m_lblLow->setText("");
+    m_lblLow->setFrameShape(QFrame::Box);
+
+    verticalLayout->addWidget(m_lblHigh);
+    verticalLayout->addWidget(m_lblLow);
+
+    // 随即生成温度
+    updateTemp();
+
+    // 安装事件过滤器
+    m_lblHigh->installEventFilter(this);
+    m_lblLow->installEventFilter(this);
+}
+
+bool PaintWidget::eventFilter(QObject* watched, QEvent* event)
+{
+    if (event->type() == QEvent::Paint) {
+        if (watched == m_lblHigh) {
+            qDebug() << "paint m_lblHigh";
+            paintHigh();
+        }
+        if (watched == m_lblLow) {
+            qDebug() << "paint m_lblLow";
+            paintLow();
+        }
+    } else if (event->type() == QEvent::MouseButtonDblClick) {
+        updateTemp();
+    }
+
+    return QWidget::eventFilter(watched, event);
+}
+
+void PaintWidget::paintHigh()
+{
+    QPainter painter(m_lblHigh);
+    // 抗锯齿
+    painter.setRenderHint(QPainter::Antialiasing, true);
+
+    // 1. 计算x轴坐标
+    int pointX[7] = {0};
+    for (int i = 0; i < 7; i++) {
+        pointX[i] = m_lblHigh->pos().x() + PADDING + (m_lblHigh->width() - PADDING * 2) / 6 * i;
+    }
+
+    // 2. 计算温度平均值
+    int tempSum     = 0;
+    int tempAverage = 0;
+    for (int i = 0; i < 7; i++) {
+        tempSum += m_highTemp[i];
+    }
+
+    tempAverage = tempSum / 7;
+
+    // 3. 计算y轴坐标
+    int pointY[7] = {0};
+    // 将标签分为一半
+    int yCenter   = m_lblHigh->height() / 2;
+    int increment = m_lblHigh->height() / 20;
+
+    for (int i = 0; i < 7; i++) {
+        // 在窗口中，y轴是向下增长
+        // 当温度比平均温度高，温度向上增长，但y轴向上减少
+        pointY[i] = yCenter + (-(m_highTemp[i] - tempAverage) * increment);
+    }
+
+    // 4. 开始绘制
+    // 初始化画笔
+    QPen pen = painter.pen();
+    pen.setWidth(3);                  // 设置画笔宽度为3
+    pen.setColor(QColor(255, 0, 0));  // 设置画笔颜色
+
+    painter.setPen(pen);
+    painter.setBrush(QColor(255, 0, 0));  // 设置画刷颜色
+    painter.setFont(QFont("Microsoft YaHei", 14));
+
+    // 画点、写文本
+    for (int i = 0; i < 7; i++) {
+        painter.drawEllipse(QPoint(pointX[i], pointY[i]), POINT_RADIUS, POINT_RADIUS);
+        painter.drawText(QPoint(pointX[i] - TEXT_OFFSET_X, pointY[i] - TEXT_OFFSET_Y), QString::number(m_highTemp[i]) + "°");
+    }
+    qDebug() << "draw here...";
+
+    // 绘制曲线
+    for (int i = 0; i < 6; i++) {
+        if (i == 0) {
+            pen.setStyle(Qt::DotLine);  // 虚线
+            painter.setPen(pen);
+        } else {
+            pen.setStyle(Qt::SolidLine);  // 实线
+            painter.setPen(pen);
+        }
+        painter.drawLine(pointX[i], pointY[i], pointX[i + 1], pointY[i + 1]);
+    }
+}
+
+void PaintWidget::paintLow()
+{
+    QPainter painter(m_lblLow);
+    // 抗锯齿
+    painter.setRenderHint(QPainter::Antialiasing, true);
+
+    // 1. 计算x轴坐标
+    int pointX[7] = {0};
+    for (int i = 0; i < 7; i++) {
+        pointX[i] = m_lblLow->x() + PADDING + (m_lblLow->width() - PADDING * 2) / 6 * i;
+    }
+
+    // 2. 计算温度平均值
+    int tempSum     = 0;
+    int tempAverage = 0;
+    for (int i = 0; i < 7; i++) {
+        tempSum += m_lowTemp[i];
+    }
+
+    tempAverage = tempSum / 7;
+
+    // 3. 计算y轴坐标
+    int pointY[7] = {0};
+    // 将标签分为一半
+    int yCenter   = m_lblLow->height() / 2;
+    int increment = m_lblLow->height() / 20;
+
+    for (int i = 0; i < 7; i++) {
+        // 在窗口中，y轴是向下增长
+        // 当温度比平均温度高，温度向上增长，但y轴向上减少
+        pointY[i] = yCenter + (-(m_lowTemp[i] - tempAverage) * increment);
+    }
+
+    // 4. 开始绘制
+    // 初始化画笔
+    QPen pen = painter.pen();
+    pen.setWidth(3);                  // 设置画笔宽度为3
+    pen.setColor(QColor(0, 0, 255));  // 设置画笔颜色
+
+    painter.setPen(pen);
+    painter.setBrush(QColor(255, 0, 0));  // 设置画刷颜色
+    painter.setFont(QFont("Microsoft YaHei", 15));
+
+    // 画点、写文本
+    for (int i = 0; i < 7; i++) {
+        painter.drawEllipse(QPoint(pointX[i], pointY[i]), POINT_RADIUS, POINT_RADIUS);
+        painter.drawText(QPoint(pointX[i] - TEXT_OFFSET_X, pointY[i] - TEXT_OFFSET_Y), QString::number(m_lowTemp[i]) + "°");
+    }
+
+    // 3.3 绘制曲线
+    for (int i = 0; i < 6; i++) {
+        if (i == 0) {
+            pen.setStyle(Qt::DotLine);  // 虚线
+            painter.setPen(pen);
+        } else {
+            pen.setStyle(Qt::SolidLine);  // 实线
+            painter.setPen(pen);
+        }
+        painter.drawLine(pointX[i], pointY[i], pointX[i + 1], pointY[i + 1]);
+    }
+}
+
+void PaintWidget::updateTemp()
+{
+    for (int i = 0; i < 7; i++) {
+        // 生成一个32位随机数并返回。
+        m_highTemp[i] = 20 + QRandomGenerator::global()->generate() % 10;
+        m_lowTemp[i]  = -5 + QRandomGenerator::global()->generate() % 10;
+    }
+
+    // 更新标签控件，重绘标签，框架发送 QEvent::Paint 事件给标签
+    m_lblHigh->update();
+    m_lblLow->update();
 }
--- src/base/4_event/paint_widget.h
+++ src/base/4_event/paint_widget.h
@@ -1,15 +1,32 @@
 ﻿#ifndef PAINT_WIDGET_H
 #define PAINT_WIDGET_H
 
+#include <QLabel>
 #include <QWidget>
 
 class PaintWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit PaintWidget(QWidget *parent = nullptr);
+    explicit PaintWidget(QWidget* parent = nullptr);
 
-signals:
+protected:
+    bool eventFilter(QObject* watched, QEvent* event) override;
+
+private:
+    // 绘制高低温曲线
+    void paintHigh();
+    void paintLow();
+
+    // 更新高低温
+    void updateTemp();
+
+private:
+    QLabel* m_lblHigh;
+    QLabel* m_lblLow;
+
+    int m_highTemp[7] = {0};
+    int m_lowTemp[7]  = {0};
 };
 
-#endif // PAINT_WIDGET_H
+#endif  // PAINT_WIDGET_H
