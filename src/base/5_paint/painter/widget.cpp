97943c2e8bf4f3a0ec0a29cf43258cf011bdb21c
yhs-coder <yhs6666@outlook.com>
2025-07-24 23:58:55 +0800

    初始化绘画界面基本数据

--- src/base/5_paint/painter/widget.cpp
+++ src/base/5_paint/painter/widget.cpp
@@ -1,14 +1,34 @@
 ﻿#include "widget.h"
+
 #include "./ui_widget.h"
 
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget)
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    initUI();
 }
 
-Widget::~Widget()
+Widget::~Widget() { delete ui; }
+
+void Widget::initUI()
 {
-    delete ui;
+    // 1. 形状
+    ui->cboShape->addItem("Point", static_cast<int>(_Point));
+    ui->cboShape->addItem("Line", static_cast<int>(_Line));
+
+    ui->cboShape->addItem("Rectangle", static_cast<int>(_Rect));
+    ui->cboShape->addItem("Rounded Rectangle", static_cast<int>(_RoundedRect));
+    ui->cboShape->addItem("Ellipse", static_cast<int>(_Ellipse));
+
+    ui->cboShape->addItem("Arc", static_cast<int>(_Arc));
+    ui->cboShape->addItem("Pie", static_cast<int>(_Pie));
+    ui->cboShape->addItem("Chord", static_cast<int>(_Chord));
+
+    ui->cboShape->addItem("Polyline", static_cast<int>(_Polyline));
+    ui->cboShape->addItem("Polygon", static_cast<int>(_Polygon));
+
+    ui->cboShape->addItem("Path", static_cast<int>(_Path));
+
+    ui->cboShape->addItem("Text", static_cast<int>(_Text));
+    ui->cboShape->addItem("Pixmap", static_cast<int>(_Pixmap));
 }
--- src/base/5_paint/painter/widget.h
+++ src/base/5_paint/painter/widget.h
@@ -3,13 +3,34 @@
 
 #include <QWidget>
 
-#include "paintwidget.h"
-
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class Widget;
 }
 QT_END_NAMESPACE
+
+// 形状的类型
+enum Shape
+{
+    _Point,  // 点
+    _Line,   // 直线
+
+    _Rect,         // 矩形
+    _RoundedRect,  // 圆角矩形
+    _Ellipse,      // 椭圆
+
+    _Polyline,  // 折线
+    _Polygon,   // 多边形
+
+    _Arc,    // 弧
+    _Pie,    // 扇形
+    _Chord,  // 弦
+
+    _Path,  // 路径
+
+    _Text,   // 文本
+    _Pixmap  // 像素图/图片
+};
 
 class Widget : public QWidget
 {
@@ -18,6 +39,10 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+private:
+    // 初始化界面
+    void initUI();
 
 private:
     Ui::Widget *ui;
