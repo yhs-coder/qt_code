89bf1e49e1696b33a00a06758098c2d0ebd60eb6
yhs-coder <yhs6666@outlook.com>
2025-07-26 00:27:18 +0800

    使用translate平移坐标系,循环保留、绘制、还原点线，使整个窗口填满点、线

--- src/base/5_paint/painter/paintwidget.cpp
+++ src/base/5_paint/painter/paintwidget.cpp
@@ -26,12 +26,25 @@ void PaintWidget::paintEvent(QPaintEvent *event)
     // 参数为绘图设备，this表示在当前窗口绘制
     QPainter painter(this);
 
-    switch (m_shape) {
-        case _Point:
-            painter.drawPoints(points, 4);
-            break;
-        case _Line:
-            painter.drawLine(points[0], points[2]);
-            break;
+    // 整个paintwidget窗口填满绘制的形状
+    for (int x = 0; x < this->width(); x += 100) {
+        for (int y = 0; y < this->height(); y += 100) {
+            // 保留当前painter状态 （当前painter状态入栈））
+            // 即（0，0)，（0，100）
+            //  (100，0)，(100,100)
+            painter.save();
+            // 按给定的偏移量转换坐标系
+            painter.translate(x, y);
+            switch (m_shape) {
+                case _Point:  // 点
+                    painter.drawPoints(points, 4);
+                    break;
+                case _Line:  // 线
+                    painter.drawLine(points[0], points[2]);
+                    break;
+            }
+            // 恢复painter之前的状态 （painter状态出栈）
+            painter.restore();
+        }
     }
 }
