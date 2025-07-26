7c7323023c39683b8a462a7416a52503e17b7231
yhs-coder <yhs6666@outlook.com>
2025-07-27 00:08:18 +0800

    绘制圆/椭圆、圆弧、饼图、弦图

--- src/base/5_paint/painter/paintwidget.cpp
+++ src/base/5_paint/painter/paintwidget.cpp
@@ -33,6 +33,14 @@ void PaintWidget::paintEvent(QPaintEvent *event)
     // 在左上角的顶点(10,20)
     QRect rect(10, 20, 80, 60);
 
+    QRect rect2(10, 10, 80, 80);
+
+    // Qt 为了支持“半度”精度，把 1° 定义成 16 个单位
+    // 30° 要写成 30*16
+    // 90° 要写成 90*16
+    int startAngle = 0 * 16;
+    int arcLength  = 90 * 16;
+
     // 整个paintwidget窗口填满绘制的形状
     for (int x = 0; x < this->width(); x += 100) {
         for (int y = 0; y < this->height(); y += 100) {
@@ -63,6 +71,27 @@ void PaintWidget::paintEvent(QPaintEvent *event)
                     // 当指定为AbsoluteSize，则2，3参数为圆角大小
                     // 当指定为RelativeSize，则2，3参数为圆角百分比
                     painter.drawRoundedRect(rect, 25, 25, Qt::RelativeSize);
+                    break;
+                case _Ellipse:  // 园、椭圆
+                    // painter.drawEllipse(10, 20, 80, 60);  // 指定矩形的左上角以及宽高
+                    // painter.drawEllipse(QPoint(50, 50), 40, 30);  // 指定中心点和rx ry
+                    // painter.drawEllipse(rect);  					// 直接指定指定矩形
+                    painter.drawEllipse(QPoint(50, 50), 40, 40);  // 圆
+                    break;
+                case _Arc:  // 圆弧
+                    // 将80*80的矩形当成外接矩形，在该矩形中画圆弧
+                    // startAngle = 30 * 16表示起始角度，是从矩形中心向右为0°，逆时针为正方向
+                    // arcLength = 120*16 表示扫过角度：从起始角再逆时针转 120°，因此弧从 30° 画到 150°。
+                    painter.drawRect(rect2);
+                    painter.drawArc(rect2, startAngle, arcLength);
+                    break;
+                case _Pie:  // 饼图
+                    painter.drawRect(rect2);
+                    painter.drawPie(rect2, startAngle, arcLength);
+                    break;
+                case _Chord:  // 弦图
+                    painter.drawRect(rect2);
+                    painter.drawChord(rect2, startAngle, arcLength);
                     break;
             }
             // 恢复painter之前的状态 （painter状态出栈）
