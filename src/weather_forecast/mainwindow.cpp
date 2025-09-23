1cbc6d408478f2b08f25ce02e040f274c7ec4e8d
yhs-coder <yhs6666@outlook.com>
2025-09-23 23:58:15 +0800

    窗口属性设置

--- src/weather_forecast/mainwindow.cpp
+++ src/weather_forecast/mainwindow.cpp
@@ -1,14 +1,13 @@
 ﻿#include "mainwindow.h"
+
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->setWindowFlag(Qt::FramelessWindowHint);  // 设置窗口无边框
+    this->setFixedSize(width(), height());         // 设置固定窗口大小
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
