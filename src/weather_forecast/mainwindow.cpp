a5099af0baae9040bd844d17eece3fc346fcd4bf
yhs-coder <yhs6666@outlook.com>
2025-09-26 00:00:51 +0800

    添加资源文件

--- src/weather_forecast/mainwindow.cpp
+++ src/weather_forecast/mainwindow.cpp
@@ -8,11 +8,23 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 
     this->setWindowFlag(Qt::FramelessWindowHint);  // 设置窗口无边框
     this->setFixedSize(width(), height());         // 设置固定窗口大小
+
+    // 构建右键菜单
+    mExitMenu = new QMenu(this);
+    mExitAct  = new QAction(this);
+
+    mExitAct->setText("退出");
+    mExitAct->setIcon(QIcon(":/res/close.png"));
+
+    mExitMenu->addAction(mExitAct);
+
+    connect(mExitAct, QAction::triggered, this, [=] { qApp->exit(0); });
 }
 
 MainWindow::~MainWindow() { delete ui; }
 
 void MainWindow::contextMenuEvent(QContextMenuEvent *event)
 {
-
+    // 弹出右键菜单
+    mExitMenu->exec(QCursor::pos());
 }
